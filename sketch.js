b35de75ebaac491c432bb56fc2727000ed13de46
Tamanna <thecreativefeather725@gmail.com>
2021-04-22 09:57:25 +0530

    bhh

--- sketch.js
+++ sketch.js
@@ -5,8 +5,8 @@ const Constraint = Matter.Constraint;
 
 var backgroundImg, person, pImg;
 var engine, world;
-var maxSnow = 100;
-var snow= [];
+var maxSnow = 5;
+var snow, createSnow= [];
 
 
 function preload() {
@@ -22,22 +22,27 @@ function setup() {
   person = createSprite(700, 300, 50, 50);
   person.addImage(pImg);
   person.scale=0.25
-  snow = new ani(random(50,400),random(50,400));
+  snow = new ani(random(50,700),random(50,300));
+  if(frameCount % 200 === 0){
+
+    for(var i=0; i<maxSnow; i++){
+        createSnow.push(new ani(random(50,700), random(50,300)));
+    }
+
+}
 }
 
 function draw() {
   background(backgroundImg);
   Engine.update(engine);
-  keyPressed();
-  drawSprites();
+  
     
-  snow.display();
-  for (var i =0; i<maxSnow; i++){
-    snow.push(new ani(random(50,400),random(50,400)))
-  }
-  if ((snow.body.position.y > snow.body.height) && (frameCount % 2 == 0) ){
-    Matter.Body.setPosition(snow.body, {x:random(50,400), y:random(50,400)})
-  }
+  for(var i = 0; i<maxSnow; i++){
+    createSnow[i].display();
+    createSnow[i].updateY()  
+}
+keyPressed();
+drawSprites();
   
   
 }
--- snow.js
+++ snow.js
@@ -1,20 +1,36 @@
-class ani {
+class ani{
     constructor(x,y){
-        this.body = Bodies.rectangle(random(10,390), 50, 0.25, 0.25);
-        this.width = width;
-        this.height = height;
-        this.image = loadImage("snow4.webp");
+        var options = {
+            'density':1,
+            'friction': 0.5,
+            'restitution':1
+        }
+        
+        this.body = Bodies.circle(x,y,5,options)
+        this.x = x;
+        this.y = y;
+        this.radius = 5
         World.add(world, this.body);
-    }
-    display(){
-        var angle = this.body.angle;
-        push();
-        translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
-        imageMode(CENTER);
-        image(this.image, 0, 0, this.width, this.height);
-        pop();
         
     }
+
+
     
+updateY(){
+    if(this.body.position.y > height){
+        Matter.Body.setPosition(this.body,{x:random(50,700),y:random(0,400)})
+    }
+}
+
+    display(){
+        
+        push()
+        noStroke();
+        fill(rgb(255,255,255));
+        ellipseMode(RADIUS);
+        ellipse(this.body.position.x, this.body.position.y, this.radius);
+        //ellipse(this.body, 0, 0, 20);                
+        pop();
+       
+    }
 }
